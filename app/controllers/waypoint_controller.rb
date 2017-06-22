dc46113a22c0ed455573a4c0fc9fb1213208cae4
aniketpawar92 <aniket.pawar@click2cloud.net>
2017-06-22 13:20:08 +0530

    update controller

--- app/controllers/waypoint_controller.rb
+++ app/controllers/waypoint_controller.rb
@@ -60,9 +60,9 @@ class WaypointController < ApplicationController
                            start_time time without time zone, \
                            end_time time without time zone, \
                            location geography(Point)); \
-                           ALTER TABLE public.engagements OWNER TO "+ pgAdminUser +"; \
+                           ALTER TABLE public.engagements OWNER TO "+ $pgAdminUser +"; \
                            CREATE SEQUENCE engagements_loc_id_seq START WITH 1 INCREMENT BY 1 NO MINVALUE NO MAXVALUE CACHE 1; \
-                           ALTER TABLE public.engagements_loc_id_seq OWNER TO "+ pgAdminUser +"; \
+                           ALTER TABLE public.engagements_loc_id_seq OWNER TO "+ $pgAdminUser +"; \
                            ALTER SEQUENCE engagements_loc_id_seq OWNED BY engagements.loc_id; \
                            ALTER TABLE ONLY engagements ALTER COLUMN loc_id SET DEFAULT nextval('engagements_loc_id_seq'::regclass); \
                            ALTER TABLE ONLY engagements ADD CONSTRAINT engagements_pkey PRIMARY KEY (loc_id); \
